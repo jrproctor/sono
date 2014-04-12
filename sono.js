74ddc16a1e0d81afd65334068c0e517cf407d51a
Jeff Proctor <jrproctor@gmail.com>
2014-04-12 10:32:07 -0700

    Add automatic generation modes

--- sono.js
+++ sono.js
@@ -4,6 +4,11 @@ sono.context =  0;
 
 sono.config = {
 	canvasId: 'sono-canvas',
+  drawDuration: 1000,
+  fuzzyRadius: 0.1,
+  fuzzyLength: 0.2,
+  colorShiftStep: 10,
+  colorShiftFuzziness: 0.5
 }
 
 sono.ColorShift = function() {
@@ -16,8 +21,7 @@ sono.ColorShift.prototype.toRGB = function() {
 
 
 sono.ColorShift.prototype.shiftIndex = function(index) {
-  var step = 20;
-  var delta = step * (Math.random() * 0.5 + 0.5);
+  var delta = sono.config.colorShiftStep * sono.upperRandom(sono.config.colorShiftFuzziness);
   if (Math.random() > 0.5) {
     delta = delta * -1;
   }
@@ -55,6 +59,7 @@ sono.Circle.prototype.draw = function() {
   this.color = new sono.ColorShift();
   this.thetas = [];
   this.currentTheta = 0;
+  this.delay = sono.config.drawDuration / this.number;
 	for(var i = 0; i < this.number; i++) {
 		this.thetas.push(Math.random() * Math.PI * 2);
   };
@@ -70,8 +75,8 @@ sono.Circle.prototype.drawCurrentTheta = function() {
   var theta = this.thetas[this.currentTheta];
 
   var rightAngle = Math.PI/2;
-  var fuzzyRadius = this.radius * (Math.random() * 0.2 + 0.8);
-  var fuzzyLength = this.length * (Math.random() * 0.2 + 0.8);
+  var fuzzyRadius = this.radius * sono.upperRandom(sono.config.fuzzyRadius);
+  var fuzzyLength = this.length * sono.upperRandom(sono.config.fuzzyLength);
 
   var a = fuzzyRadius * Math.cos(theta);
   var b = fuzzyRadius * Math.sin(theta);
@@ -95,14 +100,22 @@ sono.Circle.prototype.drawCurrentTheta = function() {
   this.currentTheta++;
   if (this.currentTheta < this.thetas.length) {
     var _this = this;
-    setTimeout(function() { _this.drawCurrentTheta(); }, 50);
+    setTimeout(function() { _this.drawCurrentTheta(); }, this.delay);
   }
 };
 
 
 sono.symmetricRandom = function(x) {
 	return Math.random() * x * 2 - x;
-}
+};
+
+sono.rangeRandom = function(from, to) {
+  return Math.random() * (to - from) + from;
+};
+
+sono.upperRandom = function(range) {
+  return Math.random() * range + (1 - range);
+};
 
 sono.LineCluster = function (x1, y1, x2, y2, width, density) {
 	this.x1 = x1;
@@ -111,7 +124,8 @@ sono.LineCluster = function (x1, y1, x2, y2, width, density) {
 	this.y2 = y2;
 	this.width = width;
 	this.density = density;
-}
+};
+
 sono.LineCluster.prototype.draw = function() {
 	
 	var number = this.width * this.density;
@@ -129,13 +143,50 @@ sono.LineCluster.prototype.draw = function() {
 	}
 }
 
+sono.randomCircle = function(x, y) {
+  var buffer = 20.0;
+  var radius = sono.rangeRandom(50, 200);
+  var length = sono.rangeRandom(radius/4, radius/2);
+  var n = Math.floor(radius);
+
+  if (!defined(x)) {
+    x = sono.rangeRandom(radius + buffer, sono.context.canvas.width - radius - buffer);
+  }
+  if (!defined(y)) {
+    y = sono.rangeRandom(radius + buffer, sono.context.canvas.height - radius - buffer);
+  }
+
+  var newcircle = new sono.Circle(radius, length, x, y, n);
+  newcircle.draw();
+
+  setTimeout(function() { sono.randomCircle(); }, sono.rangeRandom(2000, 4000));
+};
+
+sono.centeredCircle = function() {
+  var x = sono.context.canvas.width / 2.0;
+  var y = sono.context.canvas.height / 2.0;
+
+  var buffer = 20.0;
+  var radius = sono.rangeRandom(10, Math.min(x, y) - buffer);
+  var length = sono.rangeRandom(radius/4.0, radius/2.0);
+  var n = Math.floor(radius/2.0);
+
+  var newcircle = new sono.Circle(radius, length, x, y, n);
+  newcircle.draw();
+
+  setTimeout(function() { sono.centeredCircle(); }, sono.rangeRandom(500, 1000));
+};
+
 
 sono.clickHandler = function(e) {
-   var x = e.pageX - this.offsetLeft;
-   var y = e.pageY - this.offsetTop;
+  var radius = sono.rangeRandom(20, 200);
+  var length = sono.rangeRandom(radius/4, radius/2);
+  var x = e.pageX - this.offsetLeft;
+  var y = e.pageY - this.offsetTop;
+  var n = Math.floor(radius/2);
 
-   var newcircle = new sono.Circle(10, 10, x, y, 10);
-   newcircle.draw();
+  var newcircle = new sono.Circle(radius, length, x, y, n);
+  newcircle.draw();
 }
 
 window.onload = function() {
@@ -143,16 +194,9 @@ window.onload = function() {
 	sono.context = canvas.getContext('2d'); 
   sono.context.canvas.width = window.innerWidth;
   sono.context.canvas.height = window.innerHeight;
-	
-	var mycircle1 = new sono.Circle(50, 50, 100, 100, 140);
-	mycircle1.draw();
-
-	var mycircle2 = new sono.Circle(100, 30, 250, 200, 30);
-	mycircle2.draw();
-
-	var mycircle3 = new sono.Circle(200, 70, 500, 400, 140);
-	mycircle3.draw();
 
   canvas.addEventListener("click", sono.clickHandler);
+
+  sono.centeredCircle();
 }
 	
